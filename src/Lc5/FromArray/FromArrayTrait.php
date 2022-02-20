2b66407f1c4570153a1f722c3cc955498ee055fc
Łukasz Krzyszczak <lukasz.krzyszczak@finder.com>
2022-02-20 14:24:28 +0100

    Add copyright header

--- src/Lc5/FromArray/FromArrayTrait.php
+++ src/Lc5/FromArray/FromArrayTrait.php
@@ -1,7 +1,14 @@
 <?php
 
 declare(strict_types=1);
-
+/*
+ * This file is part of the lc5/typed-collection package.
+ *
+ * (c) Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace Lc5\FromArray;
 
 use Lc5\FromArray\Exception\InvalidArgumentException;
